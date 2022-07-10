9299823ec13024c5d6830d612981412194707491
Nikola Occidentale <nikolaoccid@gmail.com>
2022-07-11 00:39:03 +0200

    Change csrf_trusted_origins and env variables to hide secret key

--- project/settings.py
+++ project/settings.py
@@ -22,13 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@u*y0fi$5@grewmn6t(2qexd4sdd--i5t&)+!1me5p-4=u3b%#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'enrollment-demo.herokuapp.com', 'enrollment-demo.nikoc.uk']
 
+CSRF_TRUSTED_ORIGINS = ['enrollment-demo.nikoc.uk']
 
 # Application definition
 
