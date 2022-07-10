b4698b7d1179c75ff606466de4c4dacc319780f8
Nikola Occidentale <nikolaoccid@gmail.com>
2022-07-11 00:43:27 +0200

    Change csrf_trusted_origins

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'enrollment-demo.herokuapp.com', 'enrollment-demo.nikoc.uk']
 
-CSRF_TRUSTED_ORIGINS = ['enrollment-demo.nikoc.uk']
+CSRF_TRUSTED_ORIGINS = ['https://enrollment-demo.nikoc.uk', 'https://*.nikoc.uk']
 
 # Application definition
 
